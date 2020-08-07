c0c2c35bae88973a071a3854e30121d97fb5c9b4
hafid id-baha <hafidsoftdev@gmail.com>
2020-08-07 15:58:31 +0100

    create user class for orm

--- app.py
+++ app.py
@@ -5,5 +5,12 @@ DATABASE = "sqlite:///{DB}"
 
 Base = declarative_base()
 
+class user(Base):
+    __tablename__ = "user"
 
+    id = column('id',Integer,primary_key=True)
+    first_name = column("first_name",String(25))
+    last_name = column("last_name",String(25))
+    username = column("username",Integer,unique=True)
+    
 
